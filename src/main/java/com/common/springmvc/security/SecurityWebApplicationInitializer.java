0caa8fc9d26f1f9f595cd3364edfdac12845b9a8
kirkwang <kirkwangke@gmail.com>
2020-02-26 15:32:03 -0800

    adding obj and tomcat runnable

--- src/main/java/com/common/springmvc/security/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.common.springmvc.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
